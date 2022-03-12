795fc034cfc1fea8a97f882ee31df4d582ee2cba
Barnabas Illanicz <illanicz.barnabas@ma-coding.com>
2022-03-12 19:02:29 +0100

    Initial Commit

---
+++ SpotifyApp/SpotifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAppApp.swift
+//  SpotifyApp
+//
+//  Created by Illanicz Barnabás on 2022. 03. 12..
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
